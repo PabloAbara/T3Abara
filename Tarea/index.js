4e6b57439e5502046703e8aeabbf85cb3bf12758
PabloAbara <pbabara@uc.cl>
2018-05-29 15:16:26 -0400

    Version_2_working
    Limpieza de código
    1 ventana abierta a la vez, con Hover
    Alineamiento de aviones es correcto

--- Tarea/index.js
+++ Tarea/index.js
@@ -12,38 +12,10 @@ var colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
 		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
 		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
 		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
-var latitude;
-var longitude;
 var flights = [];
 var map;
 var markers = [];
-var windows = [];
-var image = 'https://maps.google.com/mapfiles/kml/shapes/airports_maps.png';
 var airports = [];
-
-var plane = {
-  path: 'M510,255c0-20.4-17.85-38.25-38.25-38.25H331.5L204,12.75h-51l63.75,204H76.5l-38.25-51H0L25.5,255L0,344.25h38.25l38.25-51h140.25l-63.75,204h51l127.5-204h140.25C492.15,293.25,510,275.4,510,255z',
-  fillColor: 'yellow',
-  fillOpacity: 0.8,
-  scale: 0.05,
-  strokeColor: 'gold',
-  strokeWeight: 1,
-  // size: 10,10
-  origin: (15,5),
-  // anchor: Point(5,5),
-};
-// airport_code
-// city
-// country
-// country_code
-
-
-// var icons = {
-//   plane: {
-//     icon: 'https://maps.google.com/mapfiles/kml/shapes/airports_maps.png',
-//   }
-// };
-
 
 function initMap() {
 
@@ -56,16 +28,16 @@ function initMap() {
     }
     // creo mapa
     map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
+      zoom: 5,
       center: {lat: airports[0]['lat'], lng: airports[0]['lng']}
     });
-    // creo pins
+    // creo marcadores de aeropuertos
     airports.forEach(function(element) {
       var marker = new mapIcons.Marker({
         position: {lat: element['lat'] , lng: element['lng']},
         icon: {
           path: mapIcons.shapes.MAP_PIN,
-          fillColor: '#00CCBB',
+          fillColor: '#106d66',
           fillOpacity: 1,
           strokeColor: '',
           strokeWeight: 0
@@ -73,7 +45,7 @@ function initMap() {
         map: map,
         map_icon_label: '<span class="map-icon map-icon-airport"></span>'
       });
-
+      // creo el contenido de la infoWindow de cada aeropuerto
       var airportString = '<div id="content">'+
       '<h2>Aeropuerto: '+ element['name'] + '</h2>'+
       '<h4>('+ element['airport_code'] + ' - ' + element['country_code'] + ')</h4>'+
@@ -81,24 +53,24 @@ function initMap() {
       '<h4>Ciudad: '+ element['city'] + '</h4>'+
       '<h4>Código de País: '+ element['country_code'] + '</h4>'+
       '</div>';
-
+      // creo la infoWindow por aeropuerto
       var airportwindow = new google.maps.InfoWindow({
         content: airportString
       });
-
-      marker.addListener('click', function() {
+      // me suscribo al hover
+      marker.addListener('mouseover', function() {
         airportwindow.open(map, marker);
       });
-
-
-
+      // me suscribo al mouseout para que desaparezca
+      marker.addListener('mouseout', function() {
+        airportwindow.close();
+      });
     });
-
-
+    // luego de trabajar sobre cada aeropuerto, trabajo sobre los flights
     socket.emit('FLIGHTS',{'a':''});
 
     socket.on('FLIGHTS',function(data){
-      console.log(data);
+      // por cada vuelo, obtengo y extraigo su información
       for (var key in data){
         flights.push({
           airline:data[key]['airline'],
@@ -112,7 +84,7 @@ function initMap() {
           seats:data[key]['seats'],
           plane:data[key]['plane']})
       }
-
+      // dibujo la linea recta entre origen y destino
       flights.forEach(function(element) {
         var line = new google.maps.Polyline({
             path: [
@@ -127,67 +99,58 @@ function initMap() {
       });
     });
   });
-
+  // finalmente, capturo constantemente recepciones de "position" de cada vuelo
   socket.on('POSITION', function(data){
-    // console.log(data);
+    // defino el símbolo a utilizar de marcador
+    var plane = {
+      path: 'M510,255c0-20.4-17.85-38.25-38.25-38.25H331.5L204,12.75h-51l63.75,204H76.5l-38.25-51H0L25.5,255L0,344.25h38.25l38.25-51h140.25l-63.75,204h51l127.5-204h140.25C492.15,293.25,510,275.4,510,255z',
+      fillColor: 'gray',
+      fillOpacity: 0.8,
+      scale: 0.07,
+      strokeColor: 'black',
+      strokeWeight: 1,
+      origin: new google.maps.Point(0,0),
+      anchor: new google.maps.Point(0,240)
+    };
+    // guardo posicion enviada
     var myLatLng = {lat: data['position'][0], lng: data['position'][1]};
     var lastPosn = null;
     var notHere = true;
+    // identifico el marcador al que corresponde
+    // si estaba, lo actualizo en su posicion
     for (var i = 0; i < markers.length; i++) {
       if(markers[i]!= null && markers[i]['code']==data['code']){
-        // markers[i]['marker'].setMap(null);
         var path_1 = markers[i]['marker'].getPosition();
+        lastPosn = {lat:markers[i]['lat'],lng:markers[i]['lng']};
         markers[i]['marker'].setPosition(myLatLng);
         var path_2 = markers[i]['marker'].getPosition();
         notHere = false;
         var temp = markers[i]['marker'].getPosition();
-        // console.log(temp);
         var heading = google.maps.geometry.spherical.computeHeading(path_1, path_2);
-        // console.log(heading);
-        // var auxIcon = {
-        //   path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
-        //   scale: 3,
-        //   rotation: heading,
-        //   // fillColor: markers[i]['color']
-        //   // strokeColor: markers[i]['color']
-        // }
         var auxIcon = plane;
         auxIcon.rotation=heading-90;
-        // auxIcon.rotation = heading;
-        // auxIcon.rotation = 45;
         markers[i]['marker'].setIcon(auxIcon);
-        // if(lastPosn!=null){
-        //   console.log(lastPosn);
-        //   console.log(myLatLng);
-        //   var heading = google.maps.geometry.spherical.computeHeading(lastPosn,myLatLng);
-
-        // }
-
-
-        // lastPosn = {lat:markers[i]['lat'], lng:markers[i]['lng']}
-        // console.log(lastPosn);
-        // markers.splice(i, 1);
         var line = new google.maps.Polyline({
             path: [
-                new google.maps.LatLng(myLatLng['lat'], myLatLng['lng']),
+                new google.maps.LatLng(lastPosn['lat'], lastPosn['lng']),
                 new google.maps.LatLng(myLatLng['lat'], myLatLng['lng']),
             ],
             strokeColor: markers[i]['color'],
             strokeOpacity: 1,
-            strokeWeight: 10,
+            strokeWeight: 15,
             map: map
         });
       }
     }
     if(notHere){
+      // si no estaba, lo creo, aunque aún no lo mostraré
       var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        // icon: {
-        //   path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
-        //   scale: 0,
-        // },
-        icon:plane,
+        icon: {
+          path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+          scale: 0,
+        },
       });
 
       var flight;
@@ -197,8 +160,8 @@ function initMap() {
           break;
         }
       }
-
-
+      // luego identifico el vuelo al que corresponde y creo una
+      // information Window
       var contentString = '<div id="content">'+
       '<h1>NºVuelo:'+ flight['code'] + '</h1>'+
       '<h4>Aerolínea: '+ flight['airline'] + '</h4>'+
@@ -211,40 +174,14 @@ function initMap() {
       var infowindow = new google.maps.InfoWindow({
         content: contentString
       });
-      marker.addListener('click', function() {
+      marker.addListener('mouseover', function() {
         infowindow.open(map, marker);
+      });
+      marker.addListener('mouseout', function() {
+        infowindow.close();
       });
       var color = colors[Math.floor(Math.random()*colors.length)];
       markers.push({color:color,marker:marker,code:data['code'],lat:myLatLng['lat'],lng:myLatLng['lng']});
-      // var line = new google.maps.Polyline({
-      //     path: [
-      //         new google.maps.LatLng(myLatLng['lat'], myLatLng['lng']),
-      //         new google.maps.LatLng(myLatLng['lat'], myLatLng['lng']),
-      //     ],
-      //     strokeColor: color,
-      //     strokeOpacity: 1,
-      //     strokeWeight: 10,
-      //     map: map
-      // });
     }
-
-
-
-    // marker.setPosition(myLatLng);
-    // var auxIcon = {url:image};
-    // if(lastPosn!=null){
-    //   console.log(lastPosn);
-    //   console.log(myLatLng);
-    //   var heading = google.maps.geometry.spherical.computeHeading(lastPosn,myLatLng);
-    //     auxIcon.rotation = heading;
-    //       marker.setIcon(auxIcon);
-    // }
-
-    // if(lastPosn!=null){
-    //   var heading = google.maps.geometry.spherical.computeHeading(lastPosn,myLatLng);
-    //   icon.rotation = heading;
-    //   marker.setIcon(icon);
-    // }
-
   });
 }
